7ba4ae0c36f83c7d4cb7665c942e901526238171
Md yasen rafe <mdyasenrafe@gmail.com>
2024-10-19 03:06:22 +0600

    access token secret issue fixed

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,7 +5,7 @@ export default {
   port: process.env.PORT,
   database_url: process.env.database_url,
   NODE_ENV: process.env.NODE_ENV,
-  access_token_secret: process.env.access_token_secret,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
   cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
   cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
