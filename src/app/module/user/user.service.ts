2cbcc0cdfe532264820603fc6b82bf8e09111066
Md yasen rafe <mdyasenrafe@gmail.com>
2024-10-26 11:48:02 +0600

    added user service

--- src/app/module/user/user.service.ts
+++ src/app/module/user/user.service.ts
@@ -1,10 +1,11 @@
-import { Types } from "mongoose";
+import { Types, startSession } from "mongoose";
 import { UserModel } from "./user.model";
 import { TUser } from "./user.types";
 import { JwtPayload } from "jsonwebtoken";
 import httpStatus from "http-status";
 import bcrypt from "bcrypt";
 import { AppError } from "../../errors/AppError";
+import QueryBuilder from "../../builder/queryBuilder";
 
 const getUserFromDB = async (id: Types.ObjectId) => {
   const result = await UserModel.findById(id);
@@ -32,7 +33,114 @@ const updateUserIntoDB = async (
   return result;
 };
 
+const addFollower = async (
+  userId: Types.ObjectId,
+  followerId: Types.ObjectId
+) => {
+  const session = await startSession();
+  session.startTransaction();
+
+  try {
+    const user = await UserModel.findById(userId).session(session);
+    if (!user) {
+      throw new AppError(httpStatus.NOT_FOUND, "User not found.");
+    }
+
+    const follower = await UserModel.findById(followerId).session(session);
+    if (!follower) {
+      throw new AppError(httpStatus.NOT_FOUND, "Follower not found.");
+    }
+
+    const isAlreadyFollowing = user.followers.includes(followerId);
+    const isAlreadyFollowedBy = follower.following.includes(userId);
+
+    if (!isAlreadyFollowing && !isAlreadyFollowedBy) {
+      await UserModel.findByIdAndUpdate(
+        userId,
+        { $push: { followers: followerId } },
+        { new: true, runValidators: true, session }
+      );
+      await UserModel.findByIdAndUpdate(
+        followerId,
+        { $push: { following: userId } },
+        { new: true, runValidators: true, session }
+      );
+    } else {
+      const errorMessage = isAlreadyFollowing
+        ? "User is already a follower."
+        : "User is already being followed.";
+      throw new AppError(httpStatus.BAD_REQUEST, errorMessage);
+    }
+
+    await session.commitTransaction();
+    return user;
+  } catch (error) {
+    await session.abortTransaction();
+    throw error;
+  } finally {
+    session.endSession();
+  }
+};
+
+const removeFollower = async (
+  userId: Types.ObjectId,
+  followerId: Types.ObjectId
+) => {
+  const session = await startSession();
+  session.startTransaction();
+
+  try {
+    const user = await UserModel.findById(userId).session(session);
+    if (!user) {
+      throw new AppError(httpStatus.NOT_FOUND, "User not found.");
+    }
+
+    const follower = await UserModel.findById(followerId).session(session);
+    if (!follower) {
+      throw new AppError(httpStatus.NOT_FOUND, "Follower not found.");
+    }
+
+    await UserModel.findByIdAndUpdate(
+      userId,
+      { $pull: { followers: followerId } },
+      { new: true, runValidators: true, session }
+    );
+
+    await UserModel.findByIdAndUpdate(
+      followerId,
+      { $pull: { following: userId } },
+      { new: true, runValidators: true, session }
+    );
+
+    await session.commitTransaction();
+    return await UserModel.findById(userId).populate("followers");
+  } catch (error) {
+    await session.abortTransaction();
+    throw error;
+  } finally {
+    session.endSession();
+  }
+};
+
+const getRandomUsers = async (query: Record<string, unknown>) => {
+  const queryBuilder = new QueryBuilder(UserModel.find(), query)
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
+
+  const meta = await queryBuilder.countTotal();
+  const result = await queryBuilder.modelQuery;
+  return {
+    result,
+    meta,
+  };
+};
+
 export const Userservices = {
   getUserFromDB,
   updateUserIntoDB,
+  addFollower,
+  removeFollower,
+  getRandomUsers,
 };
